9882b85e9ece7234e7e24e335eff2611fce5acdd
Oleksii Sholik <oleksii@sholik.dev>
2023-02-15 15:00:33 +0200

    Seed dev DB with some users

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,13 @@
 # Script for populating the database. You can run it as:
 #
 #     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     RCE.Repo.insert!(%RCE.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+
+alias RCE.Repo
+
+# make sure the users table is empty before executing the large insert to make
+# this seed script idempotent.
+if not Repo.exists?(RCE.Users.User) do
+  # This works without specifying any values because all columns in the "users"
+  # table have default values.
+  Repo.query!("INSERT INTO users SELECT FROM generate_series(1, 1000000)")
+end
